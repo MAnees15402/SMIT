d84ca5bafa8e8d9dc0d1b04ab27da0d7f26ff97f
Muhammad_Anees <muhammmadanees15402@gmail.com>
2024-03-21 17:16:07 +0500

    'Hello is Django'

--- SMIT/views.py
+++ SMIT/views.py
@@ -1,23 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def homePage(request):
-    data={
-        'title':'new page',
+def home(request):
+    
+    return render(request,'home.html')
 
-        'bdata':'this so hard langues bro',
-        'number':[10,30,50,70,20],
-        'ls':['php','java','python','javascript'],
-        'student':[{'name':'Ali','phone':92103458},
-                   {'name':'Quiser','phone':9234555543}]
-    }
-    return render(request,'index.html',data)
-
-def about_us(request):
-    return HttpResponse('Welcom to SMIT Programming Shop')
+def about(request):
+     return render(request,'about.html')
 
 
 def Course(request):
-    return HttpResponse('wlecom bro')
-def courseDetails(request,courseid):
-    return HttpResponse(courseid)
+      return render(request,'course.html')
+def projact(request):
+      return render(request,'projact.html')
+def contact(request):
+      return render(request,'contact.html')
