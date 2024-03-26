f8bd1ee38dd6d9a29b472b8f9b1e7181459899ae
Muhammad_Anees <muhammmadanees15402@gmail.com>
2024-03-26 17:20:24 +0500

    'django working'

--- new/models.py
+++ new/models.py
@@ -5,4 +5,9 @@ class Contact(models.Model):
     email =models.EmailField()
     password =models.CharField(max_length=30)
 
+    def __str__(self):
+       
+       return self.name
+
+
 # Create your models here.
